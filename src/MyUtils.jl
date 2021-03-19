3b85cafdb002889efe34c15597d4eb1cd35fda09
genkuroki <genkuroki@gmail.com>
2021-03-19 20:25:28 +0900

    rename RemoveOneOf to RemoveOneFrom

--- src/MyUtils.jl
+++ src/MyUtils.jl
@@ -3,7 +3,7 @@ module MyUtils
 include("my_threads.jl")
 include("my_distributed.jl")
 include("PrintfFunctions.jl")
-include("RemoveOneOf.jl")
+include("RemoveOneFrom.jl")
 
 using .PrintfFunctions
 export printf, sprintf
---
+++ src/RemoveOneFrom.jl
@@ -0,0 +1,28 @@
+"""
+    RemoveOneFrom(A::AbstractVector, k::Integer) <: AbstractVector
+
+Equivalent to A[[1:k-1; k+1:end]] without unnecessary memory allocation.
+
+Example
+
+```julia
+A = [1, 2, 3, 4, 5]
+R = RemoveOneFrom(A, 3)
+@show collect(R)
+# -> collect(R) = [1, 2, 4, 5]
+R[3] = 99
+@show R
+# -> R = [1, 2, 99, 5]
+@show A
+# -> A = [1, 2, 3, 99, 5]
+```
+"""
+struct RemoveOneFrom{T, A<:AbstractVector{T}, K<:Integer} <: AbstractVector{T}
+    A::A
+    k::K
+end
+
+Base.size(x::RemoveOneFrom) = (size(x.A, 1) - 1,)
+Base.getindex(x::RemoveOneFrom, i) = i < x.k ? x.A[i] : x.A[i+1]
+Base.getindex(x::RemoveOneFrom, ::Colon) = collect(x)
+Base.setindex!(x::RemoveOneFrom, v, i) = i < x.k ? (x.A[i] = v) : (x.A[i+1] = v)
