3458693222f81d46a647c8a2f7d4553af38a493f
Gen Kuroki <genkuroki@gmail.com>
2021-06-03 18:18:30 +0900

    Add savevar.jl

--- src/MyUtils.jl
+++ src/MyUtils.jl
@@ -5,6 +5,7 @@ include("my_distributed.jl")
 include("PrintfFunctions.jl")
 include("RemoveOneFrom.jl")
 include("showimg.jl")
+include("savevar.jl")
 
 using .PrintfFunctions
 export printf, sprintf
---
+++ src/savevar.jl
@@ -0,0 +1,7 @@
+savevar(fn, x) = write(fn, string(x))
+loadvar(fn) = read(fn, String) |> Meta.parse |> eval
+
+const dir_savevar = "tmp"
+fn_savevar(x::Symbol) = joinpath(dir_savevar, string(x) * ".txt")
+macro savevar(x) :(savevar($(fn_savevar(x)), $(esc(x)))) end
+macro loadvar(x) :(loadvar($(fn_savevar(x)))) end
